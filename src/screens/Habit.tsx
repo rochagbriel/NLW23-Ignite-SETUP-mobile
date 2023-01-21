4b614ef342b1c872cd378512afd7adb4e61f5bbd
Gabriel Rocha <rochagbriel@gmail.com>
2023-01-21 23:08:19 +0100

    created Habit Screen

--- src/screens/Habit.tsx
+++ src/screens/Habit.tsx
@@ -1,8 +1,53 @@
-import { View } from "react-native";
+import { ScrollView, Text, View } from "react-native";
+import { useRoute } from "@react-navigation/native";
+import { BackButton } from "../components/BackButton";
+import dayjs from "dayjs";
+import { ProgressBar } from "../components/ProgressBar";
+import { Checkbox } from "../components/Checkbox";
+
+interface Params {
+    date: string;
+}
 
 export function Habit() {
+    const route = useRoute();
+    const { date } = route.params as Params;
+
+    const parsedDate = dayjs(date);
+    const dayOfWeek = parsedDate.format('dddd');
+    const dayAndMonth = parsedDate.format("DD/MM")
+
     return (
         <View className="flex-1 bg-background px-8 pt-16">
+            <ScrollView
+            showsVerticalScrollIndicator={false}
+            contentContainerStyle={{ paddingBottom: 100 }}
+        >
+            <BackButton />
+
+            <Text className="mt-6 text-zinc-400 font-semibold text-base">
+                {dayOfWeek}
+            </Text>
+
+            <Text className=" text-white font-extrabold text-3xl">
+                {dayAndMonth}
+            </Text>
+                <ProgressBar progress={30}/>
+
+            <View className="mt-6">
+                <Checkbox
+                    title="Drink 2L water"
+                    checked={false}
+                />
+
+                <Checkbox
+                    title="Sleep 8h"
+                    checked={true}
+                />
+
+            </View>
+
+            </ScrollView>
 
         </View>
     )
--- src/screens/Home.tsx
+++ src/screens/Home.tsx
@@ -4,6 +4,7 @@ import {generateRangeDatesFromYearStart} from '../utils/generate-range-between-d
 
 import { HabitDay, DAY_SIZE } from "../components/HabitDay";
 import { Header } from "../components/Header";
+import { useNavigation } from "@react-navigation/native";
 
 const weekDays = ['Su', 'Mo', 'Tu', 'We', 'Th', 'Fr', 'Sa'];
 const datesfromYearStart = generateRangeDatesFromYearStart();
@@ -11,6 +12,8 @@ const minimumSummaryDatesSizes = 18 * 7;
 const amountOfDaysToFill = minimumSummaryDatesSizes - datesfromYearStart.length;
 
 export function Home(){
+
+    const { navigate } = useNavigation();
     return (
         <View className="flex-1 bg-background px-8 pt-16">
             <Header />
@@ -38,6 +41,7 @@ export function Home(){
                 datesfromYearStart.map(date => (
                     <HabitDay
                         key={date.toISOString()}
+                        onPress={() => navigate('habit', { date: date.toISOString() })}
                     />
                 ))
             }
